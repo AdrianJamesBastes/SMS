1671b27a5df8c7bf9fd7eb82e893511957d1d3d1
AdrianJamesBastes <adrianjamesbastes@gmail.com>
2025-03-26 14:32:00 +0800

    added programs

---
+++ SMS.domain/programs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.domain
+{
+    internal class programs
+    {
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
